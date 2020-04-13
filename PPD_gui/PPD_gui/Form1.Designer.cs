ef0b8406025e1947e50170cf193dd0e02af3610a
StitchDex <43028733+StitchDex@users.noreply.github.com>
2020-04-13 18:22:09 +0900

    Add files via upload

---
+++ PPD_gui/PPD_gui/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace PPD_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.State_txt = new System.Windows.Forms.Label();
+            this.State_lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserNums_txt = new System.Windows.Forms.Label();
+            this.Pause_btn = new System.Windows.Forms.Button();
+            this.Identify = new System.Windows.Forms.Button();
+            this.Access = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start_btn
+            // 
+            this.Start_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Start_btn.Location = new System.Drawing.Point(669, 12);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(249, 50);
+            this.Start_btn.TabIndex = 0;
+            this.Start_btn.Text = "Start";
+            this.Start_btn.UseVisualStyleBackColor = true;
+            this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // State_txt
+            // 
+            this.State_txt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.State_txt.BackColor = System.Drawing.Color.White;
+            this.State_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.State_txt.Location = new System.Drawing.Point(758, 129);
+            this.State_txt.Name = "State_txt";
+            this.State_txt.Size = new System.Drawing.Size(160, 30);
+            this.State_txt.TabIndex = 2;
+            this.State_txt.Text = "Stop";
+            this.State_txt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // State_lbl
+            // 
+            this.State_lbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.State_lbl.Location = new System.Drawing.Point(667, 129);
+            this.State_lbl.Name = "State_lbl";
+            this.State_lbl.Size = new System.Drawing.Size(67, 30);
+            this.State_lbl.TabIndex = 3;
+            this.State_lbl.Text = "State";
+            this.State_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(667, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "UserNums";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UserNums_txt
+            // 
+            this.UserNums_txt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserNums_txt.BackColor = System.Drawing.Color.White;
+            this.UserNums_txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UserNums_txt.Location = new System.Drawing.Point(758, 177);
+            this.UserNums_txt.Name = "UserNums_txt";
+            this.UserNums_txt.Size = new System.Drawing.Size(160, 30);
+            this.UserNums_txt.TabIndex = 5;
+            this.UserNums_txt.Text = "0";
+            this.UserNums_txt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Pause_btn
+            // 
+            this.Pause_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Pause_btn.Location = new System.Drawing.Point(669, 68);
+            this.Pause_btn.Name = "Pause_btn";
+            this.Pause_btn.Size = new System.Drawing.Size(249, 50);
+            this.Pause_btn.TabIndex = 6;
+            this.Pause_btn.Text = "Pause";
+            this.Pause_btn.UseVisualStyleBackColor = true;
+            this.Pause_btn.Click += new System.EventHandler(this.Stop_btn_Click);
+            // 
+            // Identify
+            // 
+            this.Identify.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Identify.Location = new System.Drawing.Point(669, 336);
+            this.Identify.Name = "Identify";
+            this.Identify.Size = new System.Drawing.Size(249, 50);
+            this.Identify.TabIndex = 7;
+            this.Identify.Text = "IDENT";
+            this.Identify.UseVisualStyleBackColor = true;
+            // 
+            // Access
+            // 
+            this.Access.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Access.BackColor = System.Drawing.Color.White;
+            this.Access.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Access.Font = new System.Drawing.Font("휴먼둥근헤드라인", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Access.Location = new System.Drawing.Point(669, 399);
+            this.Access.Name = "Access";
+            this.Access.Size = new System.Drawing.Size(249, 96);
+            this.Access.TabIndex = 8;
+            this.Access.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 506);
+            this.Controls.Add(this.Access);
+            this.Controls.Add(this.Identify);
+            this.Controls.Add(this.Pause_btn);
+            this.Controls.Add(this.UserNums_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.State_lbl);
+            this.Controls.Add(this.State_txt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Start_btn);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label State_txt;
+        private System.Windows.Forms.Label State_lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label UserNums_txt;
+        private System.Windows.Forms.Button Pause_btn;
+        private System.Windows.Forms.Button Identify;
+        private System.Windows.Forms.Label Access;
+    }
+}
+
---
+++ PPD_gui/PPD_gui/Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using nuitrack;
+using nuitrack.issues;
+using System.Runtime.InteropServices;
+
+namespace PPD_gui
+{
+    public partial class Form1 : Form
+    {
+        private DirectBitmap _bitmap;
+        private bool _visualizeColorImage = false;
+        private bool _colorStreamEnabled = false;
+
+        private DepthSensor _depthSensor;
+        private ColorSensor _colorSensor;
+        private UserTracker _userTracker;
+        private SkeletonTracker _skeletonTracker;
+
+        private DepthFrame _depthFrame;
+        private SkeletonData _skeletonData;
+        private IssuesData _issuesData = null;
+        private OutputMode mode;
+        private OutputMode colorMode;
+
+        private bool Pause = false;
+        private bool isStart = false;
+        private bool isAccess = false;
+        public Form1()
+        {
+            try
+            {
+                Nuitrack.Init("");
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("Cannot initialize Nuitrack.");
+                throw exception;
+            }
+
+            try
+            {
+                // Create and setup all required modules
+                _depthSensor = DepthSensor.Create();
+                _colorSensor = ColorSensor.Create();
+                _userTracker = UserTracker.Create();
+                _skeletonTracker = SkeletonTracker.Create();
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("Cannot create Nuitrack module.");
+                throw exception;
+            }
+
+            _depthSensor.SetMirror(false);
+
+            // Add event handlers for all modules
+            _depthSensor.OnUpdateEvent += onDepthSensorUpdate;
+            _colorSensor.OnUpdateEvent += onColorSensorUpdate;
+            _userTracker.OnUpdateEvent += onUserTrackerUpdate;
+            _skeletonTracker.OnSkeletonUpdateEvent += onSkeletonUpdate;
+
+            Nuitrack.onIssueUpdateEvent += onIssueDataUpdate;
+
+            mode = _depthSensor.GetOutputMode();
+            colorMode = _colorSensor.GetOutputMode();
+
+            if (mode.XRes < colorMode.XRes)
+                mode.XRes = colorMode.XRes;
+            if (mode.YRes < colorMode.YRes)
+                mode.YRes = colorMode.YRes;
+
+            _bitmap = new DirectBitmap(mode.XRes, mode.YRes);
+            for (int y = 0; y < mode.YRes; ++y)
+            {
+                for (int x = 0; x < mode.XRes; ++x)
+                {
+                    _bitmap.SetPixel(x, y, Color.FromKnownColor(KnownColor.Aqua));
+                }
+            }
+
+            InitializeComponent();
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {   
+            // Set fixed form size
+            pictureBox1.MinimumSize = pictureBox1.MaximumSize = new Size(mode.XRes, mode.YRes);
+            //this.MinimumSize = this.MaximumSize = new Size(mode.XRes, mode.YRes);
+
+            // Disable unnecessary caption bar buttons
+            //this.MinimizeBox = this.MaximizeBox = false;
+
+            // Enable double buffering to prevent flicker
+            this.DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            // Run Nuitrack. This starts sensor data processing.
+            if(!isStart)
+            {
+                try
+                {
+                    Nuitrack.Run();
+                    isStart = true;
+                    State_txt.Text = "Run";
+                }
+                catch (System.Exception exception)
+                {
+                    Console.WriteLine("Cannot start Nuitrack.");
+                    throw exception;
+                }
+            }
+            else
+            {
+                Pause = false;
+                State_txt.Text = "Run";
+                pictureBox1.Refresh();
+            }
+            
+        }
+        private void onDepthSensorUpdate(DepthFrame depthFrame)
+        {
+            if (Pause) return;
+            _depthFrame = depthFrame;
+        }
+        private void onIssueDataUpdate(IssuesData issuesData)
+        {
+            if (Pause) return;
+            _issuesData = issuesData;
+        }
+
+
+        private void onColorSensorUpdate(ColorFrame colorFrame)
+        {
+            if (Pause) return;
+
+            if (!_visualizeColorImage) return;
+            _colorStreamEnabled = true;
+            
+
+            float wStep = (float)_bitmap.Width / colorFrame.Cols;
+            float hStep = (float)_bitmap.Height / colorFrame.Rows;
+
+            float nextVerticalBorder = hStep;
+
+            Byte[] data = colorFrame.Data;
+            int colorPtr = 0;
+            int bitmapPtr = 0;
+            const int elemSizeInBytes = 3;
+
+            for (int i = 0; i < _bitmap.Height; ++i)
+            {
+                if (i == (int)nextVerticalBorder)
+                {
+                    colorPtr += colorFrame.Cols * elemSizeInBytes;
+                    nextVerticalBorder += hStep;
+                }
+
+                int offset = 0;
+                int argb = data[colorPtr]
+                    | (data[colorPtr + 1] << 8)
+                    | (data[colorPtr + 2] << 16)
+                    | (0xFF << 24);
+                float nextHorizontalBorder = wStep;
+                for (int j = 0; j < _bitmap.Width; ++j)
+                {
+                    if (j == (int)nextHorizontalBorder)
+                    {
+                        offset += elemSizeInBytes;
+                        argb = data[colorPtr + offset]
+                            | (data[colorPtr + offset + 1] << 8)
+                            | (data[colorPtr + offset + 2] << 16)
+                            | (0xFF << 24);
+                        nextHorizontalBorder += wStep;
+                    }
+
+                    _bitmap.Bits[bitmapPtr++] = argb;
+                }
+            }
+        }
+        
+        private void onUserTrackerUpdate(UserFrame userFrame)
+        {
+            if (Pause) return;
+            int size = userFrame.NumUsers;
+            UserNums_txt.Text = size.ToString();
+
+            if (_visualizeColorImage && _colorStreamEnabled) return; 
+
+            if (_depthFrame == null)
+                return;
+
+            const int MAX_LABELS = 7;
+            bool[] labelIssueState = new bool[MAX_LABELS];
+            for (UInt16 label = 0; label < MAX_LABELS; ++label)
+            {
+                labelIssueState[label] = false;
+                if (_issuesData != null)
+                {
+                    FrameBorderIssue frameBorderIssue = _issuesData.GetUserIssue<FrameBorderIssue>(label);
+                    labelIssueState[label] = (frameBorderIssue != null);
+                }
+            }
+
+            float wStep = (float)_bitmap.Width / _depthFrame.Cols;
+            float hStep = (float)_bitmap.Height / _depthFrame.Rows;
+
+            float nextVerticalBorder = hStep;
+
+            Byte[] dataDepth = _depthFrame.Data;
+            Byte[] dataUser = userFrame.Data;
+            int dataPtr = 0;
+            int bitmapPtr = 0;
+            const int elemSizeInBytes = 2;
+            for (int i = 0; i < _bitmap.Height; ++i)
+            {
+                if (i == (int)nextVerticalBorder)
+                {
+                    dataPtr += _depthFrame.Cols * elemSizeInBytes;
+                    nextVerticalBorder += hStep;
+                }
+
+                int offset = 0;
+                int argb = 0;
+                int label = dataUser[dataPtr] | dataUser[dataPtr + 1] << 8;
+                int depth = Math.Min(255, (dataDepth[dataPtr] | dataDepth[dataPtr + 1] << 8) / 32);
+                float nextHorizontalBorder = wStep;
+                for (int j = 0; j < _bitmap.Width; ++j)
+                {
+                    if (j == (int)nextHorizontalBorder)
+                    {
+                        offset += elemSizeInBytes;
+                        label = dataUser[dataPtr + offset] | dataUser[dataPtr + offset + 1] << 8;
+                        if (label == 0)
+                            depth = Math.Min(255, (dataDepth[dataPtr + offset] | dataDepth[dataPtr + offset + 1] << 8) / 32);
+                        nextHorizontalBorder += wStep;
+                    }
+
+                    if (label > 0)
+                    {
+                        int user = label * 40;
+                        if (!labelIssueState[label])
+                            user += 40;
+                        argb = 0 | (user << 8) | (0 << 16) | (0xFF << 24);
+                    }
+                    else
+                    {
+                        argb = depth | (depth << 8) | (depth << 16) | (0xFF << 24);
+                    }
+
+                    _bitmap.Bits[bitmapPtr++] = argb;
+                }
+            }
+        }
+        
+
+        // Event handler for the SkeletonUpdate event
+        private void onSkeletonUpdate(SkeletonData skeletonData)
+        {
+            if (Pause) return;
+            _skeletonData = skeletonData;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Release Nuitrack and remove all modules
+            try
+            {
+                Nuitrack.onIssueUpdateEvent -= onIssueDataUpdate;
+
+                _depthSensor.OnUpdateEvent -= onDepthSensorUpdate;
+                _colorSensor.OnUpdateEvent -= onColorSensorUpdate;
+                _userTracker.OnUpdateEvent -= onUserTrackerUpdate;
+                _skeletonTracker.OnSkeletonUpdateEvent -= onSkeletonUpdate;
+
+                Nuitrack.Release();
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("Nuitrack release failed.");
+                throw exception;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Pause) return;
+            base.OnPaint(e);
+
+            // Update Nuitrack data. Data will be synchronized with skeleton time stamps.
+            try
+            {
+                Nuitrack.Update(_skeletonTracker);
+            }
+            catch (LicenseNotAcquiredException exception)
+            {
+                Console.WriteLine("LicenseNotAcquired exception. Exception: ", exception);
+                throw exception;
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("Nuitrack update failed. Exception: ", exception);
+            }
+
+            // Draw a bitmap
+            pictureBox1.Image = _bitmap.Bitmap;
+            //e.Graphics.DrawImage(_bitmap.Bitmap, new Point(0, 0));
+
+            Graphics g1 = Graphics.FromHwnd(pictureBox1.Handle);
+
+            // Draw skeleton joints
+            if (_skeletonData != null)
+            {
+                const int jointSize = 10;
+                foreach (var skeleton in _skeletonData.Skeletons)
+                {
+                    SolidBrush brush = new SolidBrush(Color.FromArgb(255 - 40 * skeleton.ID, 0, 0));
+                    foreach (var joint in skeleton.Joints)
+                    {
+                        g1.FillEllipse(brush, joint.Proj.X * _bitmap.Width - jointSize / 2,
+                                                  joint.Proj.Y * _bitmap.Height - jointSize / 2, jointSize, jointSize);
+                        
+                    }
+                }
+
+
+            }
+
+            
+            // Update Form
+            this.Invalidate();
+        }
+
+        private void Stop_btn_Click(object sender, EventArgs e)
+        {
+            Pause = true;
+            State_txt.Text = "pause";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            _visualizeColorImage = !_visualizeColorImage;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 'Z')
+            {
+                if (isAccess)
+                {
+                    Access.Text = "Access";
+                    Access.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+                }
+                else
+                {
+                    Access.Text = "Denied";
+                    Access.ForeColor = System.Drawing.Color.Red;
+                }
+                isAccess = !isAccess;
+            }
+            
+        }
+    }
+    public class DirectBitmap : IDisposable
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        protected GCHandle BitsHandle { get; private set; }
+
+        public DirectBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Bits = new Int32[width * height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, Color colour)
+        {
+            int index = x + (y * Width);
+            int col = colour.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed)
+                return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+    }
+}
